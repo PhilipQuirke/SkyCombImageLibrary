c0f62ef07d0dc2a84954c5ef2d3795c30ad87910
PhilipQuirke <philipquirkenz@gmail.com>
2025-04-22 16:17:11 +1200

    Load image folder in Flights

--- ProcessLogic/ProcessFolder.cs
+++ ProcessLogic/ProcessFolder.cs
@@ -7,13 +7,28 @@ namespace SkyCombImage.ProcessLogic
     public class ProcessFolder
     {
         // Lists files in the input directory
-        public List<string> SrtFiles = new();
-        public List<string> GpxFiles = new();
-        public List<string> JpgFiles = new();
+        public List<string> SrtFiles;
+        public List<string> GpxFiles;
+        public List<string> JpgFiles;
+        public List<string> ImageFolders;
+
+        public ProcessFolder()
+        {
+            Reset();
+        }
+
+
+        private void Reset()
+        {
+            SrtFiles = new();
+            GpxFiles = new();
+            JpgFiles = new();
+            ImageFolders = new();   
+        }
 
 
         // Function to recursively get file names in subfolders
-        private void ListFilesInSubfolders(string folderPath, string filter = "_T")
+        private void ListVideoFilesInSubfolders(string folderPath, string filter = "_T")
         {
             string filenamefilter = "*" + filter.Trim().ToLower() + "*";
             string regexPattern = "^" + Regex.Escape(filenamefilter).Replace("\\*", ".*") + "$";
@@ -43,17 +58,56 @@ namespace SkyCombImage.ProcessLogic
             string[] subfolders = Directory.GetDirectories(folderPath);
             foreach (string subfolder in subfolders)
             {
-                ListFilesInSubfolders(subfolder, filter);
+                ListVideoFilesInSubfolders(subfolder, filter);
             }
         }
 
 
-        public void ListFilesInSubfolders(RunConfig runConfig)
+        // Create a list of the video files in the input directory and subfolders
+        public void ListVideoFilesInSubfolders(RunConfig runConfig)
         {
-            SrtFiles = new();
-            GpxFiles = new();
-            JpgFiles = new();
-            ListFilesInSubfolders(runConfig.InputDirectory);
+            Reset();
+            ListVideoFilesInSubfolders(runConfig.InputDirectory);
         }
+
+
+        // Function to recursively get names of folders that contain multiple jpg files
+        private void ListImagesInSubfolders(string folderPath, string filter = "_T")
+        {
+            string filenamefilter = "*" + filter.Trim().ToLower() + "*";
+            string regexPattern = "^" + Regex.Escape(filenamefilter).Replace("\\*", ".*") + "$";
+            // List files in the current folder
+            string[] files = Directory.GetFiles(folderPath);
+            foreach (string file in files)
+            {
+                string the_file = file.ToLower();
+                if (the_file.Length < 5)
+                    continue;
+                if (!Regex.IsMatch(the_file, regexPattern, RegexOptions.IgnoreCase))
+                    continue;
+                string suffix = the_file.Substring(the_file.Length - 4, 4);
+                if (suffix == ".jpg" || suffix == ".jpeg")
+                {
+                    ImageFolders.Add(folderPath);
+                    break;
+                }
+            }
+
+            // Recursively list files in subfolders
+            string[] subfolders = Directory.GetDirectories(folderPath);
+            foreach (string subfolder in subfolders)
+            {
+                ListImagesInSubfolders(subfolder, filter);
+            }
+        }
+
+
+        // Create a list of the folders and subfolders that contain multiple jpg files
+        public void ListImageFoldersAndSubfolders(RunConfig runConfig)
+        {
+            Reset();
+            ListImagesInSubfolders(runConfig.InputDirectory);
+        }
+
     }
 }
