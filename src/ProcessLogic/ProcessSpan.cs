9897c8e48cdf74216c970e7e48ea34de70507e40
PhilipQuirke <philipquirkenz@gmail.com>
2025-08-10 16:04:18 -0700

    Move image code to src folder

---
+++ src/ProcessLogic/ProcessSpan.cs
@@ -0,0 +1,355 @@
+// Copyright SkyComb Limited 2025. All rights reserved. 
+using Accord.Math;
+using SkyCombDrone.DroneLogic;
+using SkyCombDrone.DroneModel;
+using SkyCombGround.CommonSpace;
+using SkyCombImage.ProcessModel;
+using System.Diagnostics;
+
+
+namespace SkyCombImage.ProcessLogic
+{
+    // ProcessSpan relates to either the steps in a FlightLeg OR a sequence of FlightSteps (not related to a FlightLeg).
+    // Either way, ProcessSpan analyses ProcessObjects to refine/correct the flight altitude data using FlightStep.FixAltM/FixYawDeg/FixPitchDeg.
+    public class ProcessSpan : ProcessSpanModel
+    {
+        // Parent process
+        private ProcessAll Process { get; }
+
+
+        public ProcessSpan(ProcessAll process, int spanId, List<string>? settings = null) : base(settings)
+        {
+            Process = process;
+            ProcessSpanId = spanId;
+
+            if (settings != null)
+                LoadSettings(settings);
+        }
+
+        public void AssertGood()
+        {
+            Assert(ProcessSpanId > 0, "ProcessSpan.AssertGood: Bad SpanId");
+            Assert(MinBlockId >= 0, "ProcessSpan.AssertGood: Bad MinBlockId");
+            Assert(MaxBlockId > 0, "ProcessSpan.AssertGood: Bad MaxBlockId");
+        }
+
+
+        public void DebugPrintBest(string prefix = "")
+        {
+            var answer = "";
+
+            if (BestFixAltM != 0) answer += " BestFixAltM=" + BestFixAltM.ToString();
+
+            if (answer != "")
+                Debug.Print(
+                    "CalculateSettings_FixValues:" + prefix + " " + answer +
+                    " BestSumLocnErrM=" + BestSumLocnErrM.ToString() +
+                    " BestSumHeightErrM=" + BestSumHeightErrM.ToString());
+        }
+
+
+        // Apply FixValues to theSteps and on to the ProcessObjects and their ProcessFeatures
+        public void CalculateSettings_ApplyFixValues_Core(float fixAltM, FlightStepList theSteps, ProcessObjList objs)
+        {
+            // The image associated with each leg step now covers a slightly different area
+            // Recalculate InputImageCenter Dem and Dsm based on FixAltM/FixYawDeg/FixPitchDeg
+            theSteps.CalculateSettings_FixValues(fixAltM, Process.VideoData, Process.Drone.GroundData);
+
+            foreach (var theObject in objs)
+            {
+                var theObj = theObject.Value;
+
+                // Clone the list of features (not the features themselves) claimed by the object 
+                var objectFeatures = theObj.ProcessFeatures.Clone();
+
+                // Eliminate all object summary data.
+                theObj.ResetCalcedMemberData();
+
+                // Recalc each feature - which will have a slightly different location
+                foreach (var theFeature in objectFeatures)
+                {
+                    var theFeat = theFeature.Value;
+                    theFeat.ResetCalcedMemberData();
+
+                    // This code depends on ProcessAll.VideoData.HFOVDeg, FlightStep.FixAltM, FixYawDeg and FixPitchDeg  
+                    // This code does NOT depend on FlightStep.InputImageCenter/InputImageSize/InputImageUnitVector/InputImageDemM/InputImageDsmM
+                    theFeat.CalculateSettings_LocationM_HeightM_LOS(Process.GroundData);
+
+                    theObj.ClaimFeature(theFeat);
+                }
+            }
+
+            // Calculate the revised object-list location and height errors.
+            objs.CalculateSettings();
+        }
+
+
+        // Apply FixValues to theSteps and on to the ProcessObjects and their ProcessFeatures //PQ??
+        public bool CalculateSettings_ApplyFixValues(float fixAltM, FlightStepList theSteps, ProcessObjList objs)
+        {
+            CalculateSettings_ApplyFixValues_Core(fixAltM, theSteps, objs);
+
+            // Do we see a drop in location error sum?
+            // The location error assumes the objects are mostly stationary over the time they are observed.
+            var improvementM = BestSumLocnErrM - objs.SumLocationErrM;
+            if (improvementM >= 0.1f) // at least a 10cm improvement
+            {
+                SetBest(fixAltM, objs);
+                return true;
+            }
+
+            return false;
+        }
+
+
+        // Analyse ProcessObjects in the selected steps by assuming the drone altitude is inaccurate.
+        // Apply various "Fix*" trial values to the FlightSteps, ProcessFeatures & ProcessObjects.
+        // For each trial, measure the sum of the object location errors.
+        // Lock in the legSteps.Fix* value that reduces the error most.
+        public void CalculateSettings_FixValues(FlightStepList theSteps, ProcessObjList theObjs)
+        {
+            bool optimiseFixAltM = false;
+
+            try
+            {
+                Debug.Print("CalculateSettings_FixAltM_Start");
+                NumSignificantObjects = theObjs.Count;
+
+                if ((theSteps.Count == 0) || (NumSignificantObjects == 0))
+                    return;
+
+                float altRangeM = 90;
+                float bestAltM = 0;
+                float bestAltErr = 0;
+
+
+                ResetBest();
+                CalculateSettings_ApplyFixValues(0, theSteps, theObjs);
+                OrgSumLocnErrM = BestSumLocnErrM;
+                OrgSumHeightErrM = BestSumHeightErrM;
+
+                if (optimiseFixAltM)
+                {
+                    ResetBest();
+                    for (float fixAltM = altRangeM; fixAltM >= -altRangeM; fixAltM -= 1)
+                        if (CalculateSettings_ApplyFixValues(fixAltM, theSteps, theObjs))
+                            DebugPrintBest();
+                    bestAltM = BestFixAltM;
+                    bestAltErr = BestSumLocnErrM;
+                }
+
+                // Lock in the best single value across the leg steps
+                CalculateSettings_ApplyFixValues_Core(BestFixAltM, theSteps, theObjs);
+                BestSumLocnErrM = theObjs.SumLocationErrM;
+                BestSumHeightErrM = theObjs.SumHeightErrM;
+                Process.ProcessObjects.CalculateSettings();
+
+                DebugPrintBest();
+            }
+            catch (Exception ex)
+            {
+                throw ThrowException("ProcessSpan.CalculateSettings_FixValues", ex);
+            }
+        }
+
+
+        // Summarise the blocks in this leg
+        private void SummariseSteps(FlightStepList steps)
+        {
+            if (steps.Count > 0)
+            {
+                MinStepId = steps.First().Value.StepId;
+                MaxStepId = steps.Last().Value.StepId;
+            }
+
+            ResetTardis();
+            foreach (var step in steps)
+                SummariseTardis(step.Value);
+        }
+
+
+        // Analyse ProcessObjects in the FlightLeg, assuming various inaccuracies.
+        // Lock in the FlightSteps.FixAltM/FixYawDeg/FixPitchDeg values that reduces the location wobble most.
+        public void CalculateSettings_from_FlightLeg()
+        {
+            ResetBest();
+            ResetTardis();
+
+            if (ProcessSpanId == UnknownValue)
+                return;
+
+            var legSteps = Process.Drone.FlightSteps.Steps.GetLegSteps(ProcessSpanId);
+            var theObjs = Process.ProcessObjects.FilterByLeg(ProcessSpanId);
+
+            // Feature-level dead-reckoning ground-intercept set object location and location error. Heights are set to ~0.
+            ResetBest();
+            CalculateSettings_ApplyFixValues(0, legSteps, theObjs);
+            OrgSumLocnErrM = BestSumLocnErrM;
+            OrgSumHeightErrM = BestSumHeightErrM;
+
+            if (Process.RunUI.RunTriangulate())
+            {
+                // nq  method. 
+                SpanOptimize triangulation = new(theObjs, Process.GroundData, Process.RunUI.OutputTextBox());
+
+                foreach ((var _, var theObj) in theObjs)
+                {
+                    // For each real feature in the object, 
+                    // if feature location and/or height are not specified,
+                    // assign them last-specified location and height
+                    theObj.Calculate_RealObjectFeatures_MissingLocationsAndHeights();
+
+                    // Calculate OBJECT size in square centimeters (based on maximum # hot pixels over real features).
+                    theObj.Calculate_SizeCM2();
+
+                    // Calculate the range of the OBJECT from the drone in meters (as average over real features).
+                    theObj.Calculate_AvgRangeM();
+
+                    // Calculate the OBJECT maximum heat value (of any pixel over real features).
+                    theObj.Calculate_MaxHeat();
+
+                    // Is this OBJECT significant?
+                    theObj.Calculate_Significant();
+                }
+                theObjs.CalculateSettings();
+            }
+
+            SummariseSteps(legSteps);
+        }
+
+
+        // Analyse ProcessObjects in the Block series - assuming the drone altitude is inaccurate.
+        // Lock in the FlightSteps.FixAltM/FixYawDeg/FixPitchDeg values that reduces the location most.
+        public void CalculateSettings_from_FlightSteps(int minStepId, int maxStepId)
+        {
+            try
+            {
+                ResetBest();
+                ResetTardis();
+
+                if ((Process.Drone.FlightSteps != null) && (Process.ProcessObjects.Count > 0))
+                {
+                    // Get the FlightSteps corresponding to the block range
+                    FlightStepList theSteps = new();
+                    for (int stepId = minStepId; stepId <= maxStepId; stepId++)
+                    {
+                        FlightStep theStep;
+                        if (Process.Drone.FlightSteps.Steps.TryGetValue(stepId, out theStep))
+                            theSteps.AddStep(theStep);
+                    }
+                    if (theSteps.Count >= 4)
+                    {
+                        // Get the Objects that exist inside the block range (not overlapping the block range).
+                        // We may be analyzing 30 minutes of video.
+                        // Recall that objects are ordered by the BlockId of the first feature in the object.
+                        // For speed, scan backwards through ProcessObjList
+                        // until we find an object that ends before minBlockId.
+                        var allObjs = Process.ProcessObjects;
+                        ProcessObjList theObjs = new();
+                        for (int objectId = allObjs.Last().Key; objectId >= 0; objectId--)
+                        {
+                            ProcessObject theObject;
+                            if (allObjs.TryGetValue(objectId, out theObject))
+                            {
+                                var firstFeat = theObject.FirstFeature;
+                                var lastFeat = theObject.LastRealFeature;
+                                if ((firstFeat != null) && (lastFeat != null))
+                                {
+                                    var firstStepId = firstFeat.Block.FlightStepId;
+                                    var lastRealStepId = lastFeat.Block.FlightStepId;
+
+                                    if ((firstStepId >= minStepId) && (lastRealStepId <= maxStepId))
+                                        // Object lies fully within the specified block range
+                                        theObjs.AddObject(theObject);
+
+                                    if (lastRealStepId < minStepId)
+                                        break; // We've gone back far enough
+                                }
+                            }
+
+                        }
+
+
+                        CalculateSettings_FixValues(theSteps, theObjs);
+                        SummariseSteps(theSteps);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ThrowException("ProcessSpan.CalculateSettings_from_FlightSteps", ex);
+            }
+        }
+
+
+        public int PercentOverlapWithRunFromTo(Drone drone)
+        {
+            return FlightLeg.PercentOverlapWithRunFromTo(drone, MinStepId, MaxStepId);
+        }
+        public bool OverlapsRunFromTo(Drone drone)
+        {
+            return PercentOverlapWithRunFromTo(drone) >= FlightLegModel.MinOverlapPercent;
+        }
+    }
+
+    // A list of ProcessSpan objects
+    public class ProcessSpanList : SortedList<int, ProcessSpan>
+    {
+        public ProcessSpanList()
+        {
+        }
+
+
+        public void AddSpan(ProcessSpan processSpan)
+        {
+            BaseConstants.Assert(processSpan.ProcessSpanId > 0, "ProcessSpanList.AddLeg: No Id");
+            Add(processSpan.ProcessSpanId, processSpan);
+        }
+
+
+        // Return the index of the first and last ProcessSpan overlap of this leg with the RunVideoFromS / RunVideoToS range
+        public (int, int) OverlappingSpansRange(Drone drone)
+        {
+            int firstSpanId = BaseConstants.UnknownValue;
+            int lastSpanId = BaseConstants.UnknownValue;
+
+            foreach (var span in this)
+            {
+                if (span.Value.OverlapsRunFromTo(drone))
+                {
+                    if (firstSpanId == BaseConstants.UnknownValue)
+                        firstSpanId = span.Value.ProcessSpanId;
+                    lastSpanId = span.Value.ProcessSpanId;
+                }
+                else
+                {
+                    if (firstSpanId != BaseConstants.UnknownValue)
+                        break;
+                }
+            }
+
+            return (firstSpanId, lastSpanId);
+        }
+
+
+        public void SetFixValuesAfterLoad(VideoModel videoData, Drone drone)
+        {
+            if (drone.FlightSteps == null)
+                return;
+
+            var steps = drone.FlightSteps.Steps;
+
+            foreach (var theSpan in this)
+                if (Math.Abs(theSpan.Value.BestFixAltM) > 0)
+                    for (int stepId = theSpan.Value.MinStepId; stepId <= theSpan.Value.MaxStepId; stepId++)
+                    {
+                        if (steps.TryGetValue(stepId, out var step))
+                        {
+                            step.FixAltM = theSpan.Value.BestFixAltM;
+                            step.CalculateSettings_InputImageCenterDemDsm(videoData, drone.GroundData);
+                        }
+                    }
+        }
+    }
+
+}
